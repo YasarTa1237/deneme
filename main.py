6e01b7373121235cbeeaf20ae89a9a35fb459692
YasarTa1237 <118163429+YasarTa1237@users.noreply.github.com>
2023-10-18 22:22:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import random
+Chars = "123456789"
+ 
+password_len = int(input("Şifre kaç karakterden oluşturulsun : "))
+password_count = int(input("Kaç adet şifre oluşturulsun : "))
+for x in range(0, password_count):
+        password = ""
+        for x in range(0, password_len):
+            password_char = random.choice(Chars)
+            password      = password + password_char
+        print("Random Şifreniz : " , password)
+ 
